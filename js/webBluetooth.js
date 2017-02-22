6562299ceb7211cf506591df72c94690216977b8
EBorghgraef <evert.borghgraef@gmail.com>
2017-02-22 16:47:59 +0100

    Connect to Beacon UUID

--- js/webBluetooth.js
+++ js/webBluetooth.js
@@ -6,7 +6,7 @@ document.getElementById("GetBeacons").addEventListener("click",function(){
                 {services: ['24DDF411-8CF1-440C-87CD-E368DAF9C93E']}
             ]
         }
-        navigator.bluetooth.requestDevice(options);
+        console.log(navigator.bluetooth.requestDevice(options));
     }
     else{
         console.log("No Bluetooth present");
