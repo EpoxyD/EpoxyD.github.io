fd534a71de49d36c3007edb43827501fc07c8197
EBorghgraef <evert.borghgraef@gmail.com>
2017-02-22 17:06:46 +0100

    Connect to Beacon UUID

--- js/webBluetooth.js
+++ js/webBluetooth.js
@@ -1,26 +1,12 @@
 document.getElementById("GetBeacons").addEventListener("click",function(){
-    /*if(navigator.bluetooth){
-        console.log("Bluetooth found");        }
-        let options = {
-        filters: [
-            {services: ['heart_rate']},
-            {services: [0x1802, 0x1803]},
-                {services: ['c48e6067-5295-48d3-8d5c-0395f61792b1']},
-                {name: 'ExampleName'},
-                {namePrefix: 'Prefix'}
-            ],
-        optionalServices: ['battery_service']}))
-        }
-
-navigator.bluetooth.requestDevice(options).then(function(device) {
-  console.log('Name: ' + device.name);
-  // Do something with the device.
-})
-.catch(function(error) {
-  console.log("Something went wrong. " + error);
-});
+    if(navigator.bluetooth){
+        console.log("Bluetooth found");
+        navigator.bluetooth.requestDevice({ filters: [{ name: 'RECO' }] });
     }
     else{
         console.log("No Bluetooth present");
-    }*/
+    }
 })
+
+
+//24DDF411-8CF1-440C-87CD-E368DAF9C93E
