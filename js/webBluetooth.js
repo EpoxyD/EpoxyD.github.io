00bcbc0226ed1ea5ded98d183c5dae79428f57ac
EBorghgraef <evert.borghgraef@gmail.com>
2017-02-22 16:50:52 +0100

    Check for Bluetooth

--- js/webBluetooth.js
+++ js/webBluetooth.js
@@ -1,12 +1,9 @@
 document.getElementById("GetBeacons").addEventListener("click",function(){
     if(navigator.bluetooth){
-        console.log("Bluetooth found");
-        let options = {
-            filters: [
+        console.log("Bluetooth found");        }
+        navigator.bluetooth.requestDevice(filters: [
                 {services: ['24DDF411-8CF1-440C-87CD-E368DAF9C93E']}
-            ]
-        }
-        navigator.bluetooth.requestDevice(options).then(function(device){
+            ]).then(function(device){
             console.log('Name: ' + device.name);
         })
     }
