8a7beed564b78f4de9f4df5d00380ba0037db9f1
EBorghgraef <evert.borghgraef@gmail.com>
2017-02-23 11:32:38 +0100

    Permission testing

--- js/webBluetooth.js
+++ js/webBluetooth.js
@@ -1,17 +1,28 @@
-document.getElementById("GetBeacons").addEventListener("click",function(){
-    if(navigator.bluetooth){
-        console.log("Bluetooth found");
-        
-        //navigator.bluetooth.requestDevice({ filters: [{ name: 'RECO' }] });
-        //navigator.bluetooth.requestLEScan({
-        //  options: {acceptAllAdvertisements: true,}
-        //})
-        
-        //navigator.bluetooth.requestLEScan({options:{acceptAllAdvertisements: true,}});
-    }
-    else{
-        console.log("No Bluetooth present");
-    }
+document.getElementById("GetBeacons").addEventListener("click", function () {
+    navigator.permissions.query({
+        name: "geolocation"
+    }).then(({
+            state
+        }) => {
+            switch (state) {
+            case "granted":
+                console.log("granted location access")
+                break;
+            case "prompt":
+                console.log("prompted location access")
+                break;
+            default:
+                console.log("denied location access")
+                break;
+            }
+        }
+    case "granted":
+        showLo
+    }) if (navigator.bluetooth) {
+    console.log("Bluetooth found");
+} else {
+    console.log("No Bluetooth present");
+}
 })
 
 
