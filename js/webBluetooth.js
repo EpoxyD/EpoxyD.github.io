a6913a98f92320225c7b8d0149b3a95d649d2332
EBorghgraef <evert.borghgraef@gmail.com>
2017-02-23 11:34:57 +0100

    Bug crushing

--- js/webBluetooth.js
+++ js/webBluetooth.js
@@ -2,27 +2,26 @@ document.getElementById("GetBeacons").addEventListener("click", function () {
     navigator.permissions.query({
         name: "geolocation"
     }).then(({
-            state
-        }) => {
-            switch (state) {
-            case "granted":
-                console.log("granted location access")
-                break;
-            case "prompt":
-                console.log("prompted location access")
-                break;
-            default:
-                console.log("denied location access")
-                break;
-            }
+        state
+    }) => {
+        switch (state) {
+        case "granted":
+            console.log("granted location access")
+            break;
+        case "prompt":
+            console.log("prompted location access")
+            break;
+        default:
+            console.log("denied location access")
+            break;
         }
-    case "granted":
-        showLo
-    }) if (navigator.bluetooth) {
-    console.log("Bluetooth found");
-} else {
-    console.log("No Bluetooth present");
-}
+    })
+
+    if (navigator.bluetooth) {
+        console.log("Bluetooth found");
+    } else {
+        console.log("No Bluetooth present");
+    }
 })
 
 
